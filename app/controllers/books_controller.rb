2c115302503d52df4d159fa0a05f0b9e28b5dc0a
inemurikozoo <100814669+inemurikozoo@users.noreply.github.com>
2022-05-07 18:58:20 +0000

    bookers2 session view making.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,25 @@ class BooksController < ApplicationController
     
   end
   
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
   def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path
+  end
+  
+  
+  def destroy
     
   end
+  
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/index'
-  root to: 'homes#top'
+  
   devise_for :users
-  get 'homes/about' => 'homes#about', as: "about"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :books, only:[:new, :index, :show]
+    root to: 'homes#top'
+    get 'homes/about' => 'homes#about', as: "about"
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resources :books, only:[:new, :index, :show, :edit]
+    resources :users, only:[:index, :show, :edit, :top]
 end
