04c75a3f041af696fcf899f8345ea8155dcc2940
inemurikozoo <100814669+inemurikozoo@users.noreply.github.com>
2022-05-08 08:15:09 +0000

    bookers2 about page making.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   
-  devise_for :users
-    root to: 'homes#top'
-    get 'homes/about' => 'homes#about', as: "about"
+  root to: 'homes#top'
+  
+  get 'homes/about' => 'homes#about', as: "about"
+  
+  devise_for :users,controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
+  
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    resources :books, only:[:new, :index, :show, :edit]
-    resources :users, only:[:index, :show, :edit, :top]
+  resources :users
+  resources :books, only:[:new, :index, :show, :edit]
+    
 end
